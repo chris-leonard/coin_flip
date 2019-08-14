e6b660cf3377993cccabfd999a3e4786d6ed3a4e
chris-leonard <54002499+chris-leonard@users.noreply.github.com>
2019-08-14 16:19:19 +0100

    Add multiple coin flip games with running tally

--- coin_flip.py
+++ coin_flip.py
@@ -11,16 +11,40 @@ def flip_coin():
 # Test
 #print(flip_coin())
 
-# Solicit user guess
-coin_flip_guess = input("Heads or tails? ")
-coin_flip_guess_lowercase = coin_flip_guess.lower()
 
-# Flip coin and print outcome
-coin_flip_outcome = flip_coin()
-print("The coin flip came out " + coin_flip_outcome + ".")
+# Single coin flip game with user guess
+def coin_flip_game_single():
+    coin_flip_guess = input("Heads or tails? ")
+    coin_flip_guess_lowercase = coin_flip_guess.lower()
+    coin_flip_outcome = flip_coin()
+    print("The coin flip came out " + coin_flip_outcome + ".")
+    if coin_flip_guess_lowercase == coin_flip_outcome:
+        print("Good job! Your guess was correct")
+        return 1
+    else:
+        print("Unlucky - you got it wrong this time.")
+        return 0
 
-# Tell the user if they were correct
-if coin_flip_guess == coin_flip_outcome:
-    print("Good job! Your guess was correct")
-else:
-    print("Unlucky! Why not try again?")
+# Test
+#coin_flip_game_single()
+
+# A run of coin flip games with tally
+def coin_flip_game_multiple():
+    print("Let's play the coin flip game!")
+    total_games = 0
+    total_wins = 0
+    while 1 == 1:
+        total_games += 1
+        #coin_flip_game = coin_flip_game_single()
+        total_wins += coin_flip_game_single()
+        print("You've played " + str(total_games) + " times so far and won " + str(total_wins) + " times.")
+        proceed_input = input("Would you like to play again? ")
+        proceed_input_lowercase = proceed_input.lower()
+        if proceed_input == "yes":
+            print("Great, let's play again.")
+        elif proceed_input == "no":
+            print("Okay, thanks for playing.")
+            return
+
+# Test
+coin_flip_game_multiple()
